76043c8da2612bc26dd6c3926d33fa50e69b599c
bminor21 <bminor@bgsu.edu>
2019-06-13 15:43:36 -0400

    fixed logic

--- WebScraping/tgcplayer.py
+++ WebScraping/tgcplayer.py
@@ -9,30 +9,27 @@ card_list = []
 i = 1
 
 while True:
-    print("Page : " + str(i))
+    print("Parsing Page : " + str(i))
     r = requests.get(url + str(i),
                      headers={
                          'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
     soup = BeautifulSoup(r.content, 'html.parser')
-    cards = soup.find_all("div", {"class": "product__card"})
+    cards = soup.find_all("div", {"class": "product"})
 
     if len(cards) == 0:
         break
 
-    dict = {}
     for card in cards:
-        dict["Name"] = card.find("a", {"class": "product__name"}).text
+        d = {}
+        d["Name"] = card.find("a", {"class": "product__name"}).text
 
         for epf in card.find_all("span", {"class": "product__extended-field"}):
-            if epf is None:
-                continue
-            if "Number" in epf.text:
-                dict["Number"] = epf.text.replace("Number ", "")
+            if epf is not None and "Number" in epf.text:
+                d["Number"] = epf.text.replace("Number ", "")
                 break
 
-        dict["Price"] = card.find("dl", {"class": "product__market-price"}).find("dd").text
-
-    card_list.append(dict)
+        d["Price"] = card.find("dl", {"class": "product__market-price"}).find("dd").text
+        card_list.append(d)
     i += 1
 
 df = pandas.DataFrame(card_list)
